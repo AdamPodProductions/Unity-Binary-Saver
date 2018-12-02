d76ac264f63d3a07f3e8c30eb99cf6ac356cf087
Adam Pod <adam.podoxin@gmail.com>
2018-12-02 14:12:43 -0800

    Made filename a parameter in save and load methods

--- Assets/Scripts/UnityBinarySaver.cs
+++ Assets/Scripts/UnityBinarySaver.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public static class UnityBinarySaver
 {
-    public static void SavePlayer(Player player)
+    public static void SavePlayer(Player player, string filename)
     {
         BinaryFormatter formatter = new BinaryFormatter();
 
-        string path = Application.persistentDataPath + "/player.ubs";
+        string path = Application.persistentDataPath + "/" + filename;
         FileStream stream = new FileStream(path, FileMode.Create);
 
         formatter.Serialize(stream, player);
         stream.Close();
     }
 
-    public static Player LoadPlayer()
+    public static Player LoadPlayer(string filename)
     {
-        string path = Application.persistentDataPath + "/player.ubs";
+        string path = Application.persistentDataPath + "/" + filename;
         if (File.Exists(path))
         {
             BinaryFormatter formatter = new BinaryFormatter();
