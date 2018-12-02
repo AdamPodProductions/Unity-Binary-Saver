86240d23fd4a4a92de7c9cd12adb77a7bdd84823
Adam Pod <adam.podoxin@gmail.com>
2018-12-02 14:03:33 -0800

    Created player object

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Player
+{
+    public string name = "Player";
+    public int level = 1;
+    public int health = 100;
+    public float[] position = new float[3];
+
+    public Player()
+    {
+
+    }
+
+    public Player(string name, int level, int health, Vector3 position)
+    {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+        this.position = new float[3] { position.x, position.y, position.z };
+    }
+}
