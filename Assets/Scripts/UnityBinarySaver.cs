100b9f9c4478f3fd190217ab609335fe51921d32
Adam Pod <adam.podoxin@gmail.com>
2018-12-02 14:10:49 -0800

    Created save and load system

---
+++ Assets/Scripts/UnityBinarySaver.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class UnityBinarySaver
+{
+    public static void SavePlayer(Player player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        string path = Application.persistentDataPath + "/player.ubs";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        formatter.Serialize(stream, player);
+        stream.Close();
+    }
+
+    public static Player LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/player.ubs";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            Player player = formatter.Deserialize(stream) as Player;
+            stream.Close();
+
+            return player;
+        }
+        else
+        {
+            Debug.LogError("File not found at " + path);
+            return null;
+        }
+    }
+}
